99776e366b4dd1d72c27f45060722418098e77d6
ansavin <Andrei.V.Savin@ya.ru>
2021-12-11 21:30:48 +0300

    HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
+// TelnetClient represents simple client for interacting with TCP server.
 type TelnetClient interface {
 	Connect() error
 	io.Closer
@@ -12,10 +15,54 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+type myTelnetClient struct {
+	conn    net.Conn
+	in      io.ReadCloser
+	out     io.Writer
+	timeout time.Duration
+	address string
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+var (
+	errClientClosed error = fmt.Errorf("client close the connection")
+	errServerClosed error = fmt.Errorf("server close the connection")
+)
+
+// NewTelnetClient creates new client.
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &myTelnetClient{
+		in:      in,
+		out:     out,
+		timeout: timeout,
+		address: address,
+	}
+}
+
+func (t *myTelnetClient) Connect() (err error) {
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+	return err
+}
+
+func (t *myTelnetClient) Send() error {
+	return pushToFrom(t.conn, t.in, errClientClosed)
+}
+
+func (t *myTelnetClient) Receive() error {
+	return pushToFrom(t.out, t.conn, errServerClosed)
+}
+
+func (t *myTelnetClient) Close() error {
+	return t.conn.Close()
+}
+
+func pushToFrom(dst io.Writer, src io.Reader, expectedError error) error {
+	buf := make([]byte, 128)
+
+	i, err := src.Read(buf)
+	if err != nil {
+		return expectedError
+	}
+
+	_, err = io.WriteString(dst, string(buf[:i]))
+	return err
+}
